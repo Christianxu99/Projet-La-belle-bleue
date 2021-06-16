22ce015bc3900315c5718c82aa618ff17285a9d5
Moussaillon <moussaillon@Mac-Code-16.local>
2021-06-16 16:29:13 +0200

    remplissage bdd

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -41,6 +41,37 @@ class RegistrationController extends AbstractController
                 )
             );
 
+
+
+          /** @var UploadedFile $brochureFile */
+          $brochureFile = $form->get('image')->getData();
+
+          // this condition is needed because the 'brochure' field is not required
+          // so the PDF file must be processed only when a file is uploaded
+          if ($brochureFile) {
+              $originalFilename = pathinfo($brochureFile->getClientOriginalName(), PATHINFO_FILENAME);
+              // this is needed to safely include the file name as part of the URL
+              $newFilename = uniqid().'.'.$brochureFile->guessExtension();
+
+              // Move the file to the directory where brochures are stored
+              try {
+                  $brochureFile->move(
+                      $this->getParameter('brochures_directory'),
+                      $newFilename
+                  );
+              } catch (FileException $e) {
+                  // ... handle exception if something happens during file upload
+              }
+
+              // updates the 'brochureFilename' property to store the PDF file name
+              // instead of its contents                                          
+             
+              $user->setImage($newFilename);
+          }
+
+
+
+          
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();

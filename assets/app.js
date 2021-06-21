15c2f20bac2121d3201fa89cb98ceb079c58631d
Christian XU <christian.xu99@gmail.com>
2021-06-21 11:41:14 +0200

    integration de react

---
+++ assets/app.js
@@ -0,0 +1,23 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+import React from "react";
+
+import ReactDOM from "react-dom";
+
+
+function App(){
+
+    return(
+        <div>JE SUIS UN COMPOSANT REACT</div> // on aurait pu inclure un composant comme on l'a fait pendant le cours et le charger ici <Home /> par exemple
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root")); // l'id root ici sera créé dans un <div id="root"></div> dans n'importe quel template twig que vous choisissez.
